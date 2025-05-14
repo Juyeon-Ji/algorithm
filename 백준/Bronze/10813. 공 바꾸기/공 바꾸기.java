d11eb912360b18676f2ebd1ff4c5834af4b25c81
Juyeon Ji <46771574+Juyeon-Ji@users.noreply.github.com>
2025-05-14 19:42:33 +0900

    Update 공 바꾸기.java

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,36 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    static int[] arr;
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+    public static void initBasketCount(int basketCount) {
+        arr = new int[basketCount];
+        for (int i = 0; i < basketCount; i++) {
+            arr[i] = i + 1;
+        }
+    }
 
-        int[] baskets = new int[N];
-        for (int i = 0; i < N; i++) {
-            baskets[i] = i + 1;
+    public static void changeBasketCount(int a, int b) {
+        int temp = arr[a - 1];
+        arr[a - 1] = arr[b - 1];
+        arr[b - 1] = temp;
+    }
+
+    public static void printBasketCount() {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int basketCount = sc.nextInt();
+        int changeCount = sc.nextInt();
+
+        initBasketCount(basketCount);
+
+        for (int i = 0; i < changeCount; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            changeBasketCount(a, b);
         }
 
-        for (int k = 0; k < M; k++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken()) - 1;
-            int j = Integer.parseInt(st.nextToken()) - 1;
-
-            int tmp = baskets[i];
-            baskets[i] = baskets[j];
-            baskets[j] = tmp;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int num : baskets) {
-            sb.append(num).append(' ');
-        }
-
-        System.out.println(sb.toString());
+        printBasketCount();
+        sc.close();
     }
 }
