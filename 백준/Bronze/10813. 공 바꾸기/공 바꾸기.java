91a4d3b3805892218a66f231b320d2c831bf22f2
Juyeon Ji <46771574+Juyeon-Ji@users.noreply.github.com>
2025-05-14 18:16:22 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] baskets = new int[N];
+        for (int i = 0; i < N; i++) {
+            baskets[i] = i + 1;
+        }
+
+        for (int k = 0; k < M; k++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+
+            int tmp = baskets[i];
+            baskets[i] = baskets[j];
+            baskets[j] = tmp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : baskets) {
+            sb.append(num).append(' ');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
